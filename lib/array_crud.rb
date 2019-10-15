bdb127afb68c7ef0a43c94de9645bf94336293fc
Jen Lindner <jenlindner22@gmail.com>
2019-10-15 16:07:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["cheese", "milk", "ice cream", "butter"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+cats = [element]
+cats << array
 end
 
 def add_element_to_start_of_array(array, element)
